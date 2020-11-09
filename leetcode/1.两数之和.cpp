cbe97b37053bcd1fcd2443be01386d1e9fc949af
zoomdong <1344492820@qq.com>
2020-11-09 21:34:13 +0800

    feat: add leetcode

--- leetcode/1.两数之和.cpp
+++ leetcode/1.两数之和.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
       vector<int>res;
-      
+      map<int,int>hash;
+      for (int i = 0; i < nums.size(); i ++) {
+        hash[nums[i]] = i;
+      }
+      for (int i = 0;i<nums.size();i++) {
+          int k = target - nums[i];
+          if ( hash[k] != i) {
+              res.push_back(i);
+              res.push_back(hash[k]);
+              return res;
+          }
+      }
+      return vector<int>{};
     }
 };
