b7cb57d29bc0317582e2994dac42288c778dce03
zoomdong <1344492820@qq.com>
2020-11-24 22:59:55 +0800

    feat: add 高精度乘法

---
+++ base/character-1/高精度/multiple.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<string>
+
+using namespace std;
+// C = A * b ,A >= 0 && b > 0
+
+vector<int> mul(vector<int> &A, int b) {
+  vector<int> C;
+  int t = 0;
+  for (int i = 0;i<A.size() || t; i ++) {
+    // 如果A还没有枚举完，就t加上A[i]*b的一个结果
+    if (i < A.size()) {
+      t += A[i] * b;
+    }
+    // 只需要个位来作为结果
+    C.push_back(t % 10);
+    // 进位还是用 t /= 10
+    t /= 10;
+  }
+  // 处理前导0
+  while(C.size() > 1 && C.back() == 0) {
+    C.pop_back();
+  }
+  return C;
+}
+
+int main () {
+  string a;
+  int b;
+  cin >> a >> b;
+  vector<int> A;
+  for (int i = a.size() - 1;i>=0;i--) {
+    A.push_back(a[i] - '0');
+  }
+  auto C = mul(A, b);
+  for (int i = C.size() - 1;i>=0;i--) {
+    cout << C[i];
+  }
+  return 0;
+}
