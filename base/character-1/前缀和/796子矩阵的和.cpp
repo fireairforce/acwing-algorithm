b1e6768967056baf79c163774e656480160b69d1
zoomdong <1344492820@qq.com>
2020-12-14 09:14:19 +0800

    feat: add 前缀和矩阵

---
+++ base/character-1/前缀和/796子矩阵的和.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+const int N = 1010;
+int a[N][N], s[N][N];
+int main () {
+  int n,m,q;
+  cin >> n >>m >> q;
+  for (int i = 1;i<=n;i++) {
+    for (int j = 1;j<=m;j++) {
+      cin >> a[i][j];
+    }
+  }
+  for (int i = 1;i<n;i++) {
+    for (int j = 1;j<=m;j++) {
+      // 求前缀和
+      s[i][j] = s[i-1][j] + s[i][j-1] -s[i-1][j-1] + a[i][j];
+    }
+  }
+  while (q --) {
+    int x1, y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    // 算子矩阵的和
+    cout << s[x2][y2] - s[x1- 1][y2] - s[x2][y1- 1] + s[x1-1][y1-1] << endl;
+  }
+  return 0;
+}
