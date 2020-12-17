ca02672187f0a0daddae5986812ad09a28b0035b
zoomdong <1344492820@qq.com>
2020-12-17 09:16:14 +0800

    feat: add 差分数组

--- base/character-1/差分/797差分.cpp
+++ base/character-1/差分/797差分.cpp
@@ -1,33 +1,33 @@
 #include<iostream>
 
 using namespace std;
-int a[100000] = {0}, b[100007] = {0};
 
-void insert (int l, int r, int c) {
-  b[r] += c;
-  b[l + 1] -=c;
-}
+const int N = 100000 + 7;
+int a[N], b[N];
 
 int main () {
-  int n, m;
-  cin >> n >> m;
-  for (int i = 1;i<=n;i++) {
-    cin >> a[i];
-  }
-  for (int i = 1;i<=n;i++) {
-    insert(i,i,a[i]);
-  }
-  while (m--) {
-    int l, r, x;
-    cin >> l >> r >> x;
-    insert(l, r, x);
-  }
-  // 对数组本身求个前缀和(b为差分数组)
-  for (int i = 1;i<=n;i++) {
-    b[i] += b[i-1];
-  }
-  for (int i =1;i <= n;i++) {
-    cout << b[i] << endl;
-  }
-  return 0;
+    int n,m;
+    cin >> n >> m;
+    for (int i = 1;i<=n;i++) {
+        cin >> a[i];
+    }
+    // 原数组里面的n个值记得是插入进去的
+    for (int i = 1;i<=n;i++) {
+        b[i] += a[i];
+        b[i+1] -= a[i];
+    }
+    while(m--) {
+     int l, r, x;
+     cin >> l >> r >> x;
+     b[l] += x;
+     b[r + 1] -= x;
+    }
+    // 对差分数组求一遍前缀和
+    for (int i =1;i<=n;i++) {
+        b[i] += b[i-1]; 
+    }
+    for (int i = 1;i<=n;i++) {
+        cout << b[i] << " ";
+    }
+    return 0;
 }
