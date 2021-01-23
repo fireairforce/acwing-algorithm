434c62a2039c44f3d02e956cf39c52de977d7440
zoomdong <1344492820@qq.com>
2021-01-23 22:58:53 +0800

    feat: add 堆优化dijkstra

---
+++ base/character-3/堆优化Dijkstra/堆优化djkstra.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+
+using namespace std;
+
+// 堆优化的 dijkstra 一般针对稀疏图
+const int N = 1e5 + 10, M = 2 * N;
+// 这里的 w 是用来存权重的
+int h[N], e[M], ne[M], w[M], idx = 0;
+int n, m;
+int dist[N];
+bool st[N];
+
+// 使用堆来维护节点到起点的距离
+typedef pair<int, int> PII;
+
+
+void add (int a, int b, int c) {
+  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx ++;
+}
+
+int dijkstra () {
+  memset(dist, 0x3f, sizeof dist);
+  dist[1] = 0;
+  // 使用优先队列维护, 小根堆，不指定 greater 参数就默认是大顶堆
+  // vector 表示优先队列使用什么来实现，一般使用 vector 比较合适
+  priority_queue<PII, vector<PII>, greater<PII> > heap;
+  // 1号点距离为0
+  heap.push(make_pair(0, 1));
+  // 有点像 bfs 的感觉
+  while (heap.size()) {
+    auto t = heap.top();
+    heap.pop();
+
+    int ver = t.second, distance = t.first;
+    // 如果这个点已经被计算过了，就直接跳了
+    if (st[ver]) continue;
+    // 这里的时间复杂度只有 m
+    for (int i = h[ver]; i != -1; i = ne[i]) {
+      int j = e[i];
+      if (dist[j] > distance + w[i]) {
+        dist[j] = distance + w[i];
+        heap.push(make_pair(dist[j], j));
+      }
+    }
+  }
+  if (dist[n] == 0x3f3f3f3f) {
+    return -1;
+  }
+  return dist[n];
+}
+
+int main () {
+  cin >> n >> m;
+  memset(h, -1, sizeof h);
+
+  while (m --) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    add(a, b, c);
+  }
+  int t = dijkstra();
+  cout << t << endl;
+  return 0;
+}
--- base/character-3/朴素Dijkstra/dijkstra.cpp
+++ base/character-3/朴素Dijkstra/dijkstra.cpp
@@ -8,24 +8,33 @@ using namespace std;
 const int N = 510;
 
 int n, m;
+// 领接矩阵，存图
 int g[N][N];
+// 记录起始点到其他点的最短距离
 int dist[N];
+// st存知道最短路的点
 bool st[N];
 
 int dijkstra () {
+  // 其他点为正无穷
   memset(dist, 0x3f, sizeof dist);
   // 把起始点的距离初始化为 0
   dist[1] = 0;
 
+  // 每一次循环都可以得到一个点到起点到最短距离
   for (int i = 0; i < n; i++) {
     int t = -1;
     for (int j = 1; j <= n; j++) {
+      // 从 st[j] 为 false 的点中找到 dist 最短点
       if (!st[i] && (t == -1 || dist[t] > dist[j])) {
         t = j;
       }
     }
+    // 找到最短路就退出出去
+    if (t == n) break;
     st[t] = true;
     
+    // 这里其实就是利用 t 来去更新其他点到起点的距离
     for (int j = 1; j <= n; j++) {
       dist[j] = min(dist[j], dist[t] + g[t][j]);
     }
@@ -37,15 +46,21 @@ int dijkstra () {
 
 int main () {
   cin >> n >> m;
+  // 初始化其他点为正无穷
+  // 0x3f 转换成二进制放到 g 里面会是一个很大的数字
   memset(g, 0x3f, sizeof g);
   while (m --) {
     int a, b, c;
     cin >> a >> b >> c;
+    // 重边判断一下
     g[a][b] = min(g[a][b], c);
   }
 
   int t = dijkstra();
-
-  cout << t << endl;
+  if (t == 0x3f3f3f3f) {
+    cout << -1 << endl;
+  } else {
+    cout << t << endl;
+  }
   return 0;
 }
